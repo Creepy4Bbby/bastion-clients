d3c9d7149b70cb1f8af39c7cf8a27a09015f9d49
Dolores H <dolores.huet@metanext.com>
2025-04-16 23:20:50 +0000

    Ajout des fichiers Helm et agents Teleport

--- terraform/.terraform/clients/cloud-init/ad-init.ps1
+++
@@ -1,6 +0,0 @@
-<powershell>
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName "efrei.local" `
-  -SafeModeAdministratorPassword (ConvertTo-SecureString "SuperSecure123!" -AsPlainText -Force) `
-  -Force
-</powershell>
