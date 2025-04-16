81ec98423d72d130f646db96e192763ab9d7cb6f
Dolores H <dolores.huet@metanext.com>
2025-04-16 18:22:13 +0000

    add terraform fichier

---
+++ terraform/terraform/clients/cloud-init/ad-init.ps1
@@ -0,0 +1,6 @@
+<powershell>
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName "efrei.local" `
+  -SafeModeAdministratorPassword (ConvertTo-SecureString "SuperSecure123!" -AsPlainText -Force) `
+  -Force
+</powershell>
