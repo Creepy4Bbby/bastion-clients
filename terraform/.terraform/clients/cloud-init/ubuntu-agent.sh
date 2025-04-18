13667fa6960d93c3bff0807f272ea3bfa3d356c4
Dolores H <dolores.huet@metanext.com>
2025-04-18 11:32:19 +0000

    Clean start: reset project to empty base

--- terraform/.terraform/clients/cloud-init/ubuntu-agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://cdn.teleport.dev/install-v16.4.12.sh | bash -s 16.4.12
-teleport start \
-  --roles=node \
-  --token=YOUR_TOKEN_HERE \
-  --auth-server=TELEPORT_PUBLIC_IP:3025
